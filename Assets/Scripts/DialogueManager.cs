aabeddc88abee4cf172198608b5dccdb887e3ded
mike <mich.rivera84@gmail.com>
2023-09-18 22:21:30 -0400

    rough dialogue system is in place -- implementing will take some planning

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,19 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class DialogueManager : MonoBehaviour
 {
+
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+
     public Queue<string> sentences;
 
+    private enum dialogueState
+    {
+        Prompt,
+        ClickThru,
+        End
+    }
     // Start is called before the first frame update
     void Start()
     {
         sentences = new Queue<string>();
     }
-
-    public void StartDialogue (Dialogue dialogue) {
-        print("Dialogue started with " + dialogue.name);
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            DisplayNextSentence();
+        }
     }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log("Click to talk to " + dialogue.name);
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+        Debug.Log(sentence);
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End of dialogue");
+    }
+
+
 
 }
